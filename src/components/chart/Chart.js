11dd52992331f42028c8da16af48d1c22424ebff
vinithaneyashree <vinithaneyashree2901@gmail.com>
2022-06-09 10:33:15 +0530

    admin added

---
+++ src/components/chart/Chart.js
@@ -0,0 +1,14 @@
+import "./chart.css";
+
+
+const Chart = () => {
+    return (
+        <div className="charts">
+       Charts
+ 
+        </div>
+    )
+};
+
+
+export default Chart;
---
+++ src/pages/login/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./login.css";
+
+const Login=()=>{
+    return(
+        <div className="">
+      Login
+        </div>
+      
+    )
+}
+
+
+export default  Login;
